c1fd00b61c5ae3e1e978c61d942298e5b093b8a0
Andrew Mount <83432298+andrewmount@users.noreply.github.com>
2023-12-12 00:04:18 +0000

    Update example list and questions

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "How do I hoist a sail?",
+        value: "How do I hoist a sail?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "How do I dress like a pirate?", value: "How do I dress like a pirate?" },
+    { text: "My PC won't turn on?", value: "My PC won't turn on?" }
 ];
 
 interface Props {
